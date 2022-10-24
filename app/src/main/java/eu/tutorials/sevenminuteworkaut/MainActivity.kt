51dfaa2c62679fafb4588ffd99d99eae366125f5
Bartosz Filipów <filipow.bartosz@gmail.com>
2022-10-24 16:01:16 +0200

    first commit

---
+++ app/src/main/java/eu/tutorials/sevenminuteworkaut/MainActivity.kt
@@ -0,0 +1,29 @@
+package eu.tutorials.sevenminuteworkaut
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import eu.tutorials.sevenminuteworkaut.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener{
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
